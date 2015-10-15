fe4f3cac59820b2992bcc89a1cd251b119541806
Sam Narisi <snarisi@gmail.com>
2015-10-15 17:57:30 -0400

    Refactor app.js

--- app.js
+++ app.js
@@ -1,99 +1,8 @@
 $(document).ready(function () {
-  
-  //initialize came by creating a number and setting the count to 5
   var number,
     count,
     prevGuesses;
-  
-  initialize();
-  
-  //when the user submits a guess
-  $('#submit-guess').on('click', function () {
-    
-    //make sure the game hasn't ended
-    if (count > 0) {
-      var guess = parseFloat(document.getElementById('input').value, 10),
-        distance = Math.abs(guess - number),
-        temperature,
-        direction;
       
-      //subtract one from the count
-      count -= 1;
-
-      //generate feedback
-      if (guessIsInvalid(guess)) {
-        $('.feedback').html("<p>Please enter an integer from 1 to 100.</p");
-        $('.feedback').show();
-      } else {
-        if (isRepeat(guess)) {
-          $('.feedback').html("<p>Please enter a new guess.</p");
-          $('.feedback').show();
-        } else {
-          
-          //winning sequence
-          if (guess === number) {
-            gameEnd('win!');
-
-          //losing sequence  
-          } else if (count === 0) {
-            gameEnd('lose :(');
-            $('#count').html("You have <span class='bold'>" + count + "</span> guesses left");
-
-          //regular guessing sequence
-          } else {
-            if (guess > number) {
-              direction = "<span class='bold'>lower</span>";
-            } else {
-              direction = "<span class='bold'>higher</span>";
-            }
-            if (prevGuesses.length === 0) {
-              if (distance <= 10) {
-                temperature = "<span class='red bold'>hot</span>";
-              } else if (distance <= 25) {
-                temperature = "<span class='red bold'>warm</span>";
-              } else if (distance <= 50) {
-                temperature = "<span class='blue bold'>cool</span>";
-              } else if (distance <= 75) {
-                temperature = "<span class='blue bold'>cold</span>";
-              } else {
-                temperature = "<span class='blue bold'>ice cold</span>";
-              }
-              $('.feedback').html("<p>Your guess is " + temperature + ".</p><p>Guess " +  direction + ".</p");
-            } else {
-              if (distance < Math.abs(number - prevGuesses[prevGuesses.length - 1])) {
-                if (distance < 10) {
-                  temperature = "<span class='red bold'>hot</span>";
-                } else {
-                  temperature = "<span class='red bold'>warmer</span>";
-                }
-              } else {
-                temperature= "<span class='blue bold'>colder</span>";
-              }
-              $('.feedback').html("<p>Getting " + temperature + ".</p><p>Guess " + direction + ".</p>");
-            }
-
-            //display feedback
-            $('.feedback').show();
-
-            //update count
-            if (count === 1) {
-              $('#count').html("You have <span class='bold'>" + count + "</span> guess left");
-            } else {
-              $('#count').html("You have <span class='bold'>" + count + "</span> guesses left");
-            }
-            
-            //add guess to history
-            prevGuesses.push(guess);
-
-            //update guess history
-            $('#guess-history').append('<li>' + guess + ' (' + temperature + ')</li>');
-          }
-        }
-      }
-   
-    }
-  });
-  
   //initialize game
   function initialize() {
     number = Math.floor(Math.random() * 100 + 1);
@@ -103,14 +12,86 @@ $(document).ready(function () {
     $('#end-message').hide();
     $('#answer').hide();
     $('.feedback').hide();
-    $('#count').html("You have <span class='bold'>" + count + "</span> guesses left");
+    displayCount();
     document.getElementById('input').value = "";
     $('#guess-history').hide();
     $('#guess-history').empty();
     $('.fade-on-end').removeClass('faded');
   }
   
-  //sequence when game ends
+  //show user the current guess count
+  function displayCount() {
+    if (count === 1) {
+      $('#guessOrGuesses').text('guess');
+      $('#count').text(count);
+    } else {
+      $('#guessOrGuesses').text('guesses');
+      $('#count').text(count);
+    }
+  }
+  
+  //find what direction the user should move in
+  function getDirection(guess) {
+    var dir;
+    
+    if (guess < number) {
+      dir = "higher";
+    } else {
+      dir = "lower";
+    }
+    return "Guess <span class='bold'>" + dir + "</span>.";
+  }
+  
+  //find the temperature of the guess
+  function getTemperature(guess) {
+    var distance = Math.abs(guess - number),
+      temp,
+      color;
+
+    if (prevGuesses.length === 0) {
+      if (distance <= 10) {
+        temp = "hot";
+        color = "red";
+      } else if (distance <= 25) {
+        temp = "warm";
+        color = "red";
+      } else if (distance <= 50) {
+        temp = "cool";
+        color = "blue";
+      } else if (distance <= 75) {
+        temp = "cold";
+        color = "blue";
+      } else {
+        temp = "ice cold";
+        color = "blue";
+      }
+      return "Your guess is <span class='bold " + color + "'>" + temp + "</span>.";
+    } else {
+      if (distance < Math.abs(number - prevGuesses[prevGuesses.length - 1])) {
+        temp = "warmer";
+        color = "red";
+      } else {
+        temp = "colder";
+        color = "blue";
+      }
+      return "Getting <span class='bold " + color + "'>" + temp + "</span>.";
+    }
+  }
+  
+  //display guess feedback to the user
+  function displayFeedback(temperature, direction) {
+    $('.feedback').children().remove();
+    $('.feedback').append("<p>" + temperature + "</p><p>" + direction + "</p>");
+    $('.feedback').show();
+  }
+  
+  //update guess history
+  function updateGuessHistory(guess) {
+    var description = $('.feedback').find('p').first().find('span').text();
+    $('#guess-history').append("<li>" + guess + " (" + description + ")</li>");
+  }
+  
+  //show alert when the game ends
   function gameEnd(outcome) {
     $('#win-or-lose').html(outcome);
     $('#end-message').show();
@@ -122,7 +103,7 @@ $(document).ready(function () {
     }
   }
   
-  //check if guess is an integer from 1-100
+  //make sure the guess is an integer from 1-100
   function guessIsInvalid(guess) {
     return (typeof guess !== 'number' || guess % 1 !== 0 || guess > 100 || guess < 1);
   }
@@ -131,25 +112,59 @@ $(document).ready(function () {
   function isRepeat(guess) {
     return prevGuesses.indexOf(guess) > -1;
   }
-  
-  //hit enter to submit guess
+    
+  //collect the user's guess
+  $('#submit-guess').on('click', function () {
+    
+    //make sure the game hasn't ended
+    if (count > 0) {
+      //collect guess
+      var guess = parseFloat(document.getElementById('input').value, 10),
+        temperature = getTemperature(guess),
+        direction = getDirection(guess);
+
+      //make the guess is valid and new
+      if (guessIsInvalid(guess)) {
+        $('.feedback').html("<p>Please enter an integer from 1 to 100.</p");
+        $('.feedback').show();
+      } else if (isRepeat(guess)) {
+        $('.feedback').html("<p>Please enter a new guess.</p");
+        $('.feedback').show();
+      } else {
+        count -= 1;
+        displayCount();
+
+        if (guess === number) {
+          gameEnd('win!');
+        } else if (count === 0) {
+          gameEnd('lose :(');
+        } else {
+          displayFeedback(temperature, direction);
+          prevGuesses.push(guess);
+          updateGuessHistory(guess);
+        }
+      }
+    }
+  });
+
+  //allow the user to submit guess with the enter key
   $('#input').on('keypress', function (event) {
     if (event.which === 13) {
       $('#submit-guess').trigger('click');
     }
   });
-  
-  //toggle guess history
+
+  //allow the user to toggle the guess history display
   $('#history-toggle').on('click', function () {
     $('#guess-history').toggle();
   });
   
-  //play again
+  //allow the user to click the button to play again
   $('#play-again').on('click', function () {
     initialize();
   });
   
-  //get the answer
+  //allow the user to click the button to get the answer
   $('#hint').on('click', function () {
     $('#answer').html("The answer was: <span class='bold'>" + number + "</span>").show();
     $('.game-end').show();
@@ -157,4 +172,5 @@ $(document).ready(function () {
     $('.fade-on-end').addClass('faded');
   });
 
+  initialize();
 });
